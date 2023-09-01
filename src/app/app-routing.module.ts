68984f35dddebe7e43ebba775495cda82eb5fc36
zeyad sheeha <sheehaph@gmail.com>
2023-09-02 00:05:49 +0300

    Zozz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { UserProfileComponent } from './Core/Components/user-profile/user-profile.component';
+import { MovieDetailsComponent } from './Pages/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'HomeComponent', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'movieDetails', component: MovieDetailsComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { LoginComponent } from './Core/Components/Registeration/login/login.component';
+import { SignUpComponent } from './Core/Components/Registeration/sign-up/sign-up.component';
+import { UserProfileComponent } from './Core/Components/user-profile/user-profile.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { MovieDetailsComponent } from './Pages/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    HomeComponent,
+    ErrorComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
